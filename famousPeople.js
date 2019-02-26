01911592644413f001c5354ce32e7982fa793558
CristofMorissette1 <cristofmorissette@hotmail.com>
2019-02-26 22:02:22 +0000

    Fixed up my code

--- famousPeople.js
+++ famousPeople.js
@@ -4,21 +4,26 @@ const settings = require("./settings"); // settings.json
 const client = new pg.Client(settings);
 
 const name = process.argv[2];
-
+const select = `SELECT * FROM famous_people WHERE (first_name='${name}') OR (last_name='${name}')`;
 
 client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query(`SELECT * 
-                FROM famous_people 
-                WHERE (first_name='${name}') OR (last_name='${name}')`, (err, result) => {
+  client.query(select, (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(`Searching...`);
-    console.log(`Found this result`);
-    console.log(result.rows); 
-    client.end();
+    
+    lookUpFamousPeople(result.rows);
+     client.end();
   });
 });
+
+function lookUpFamousPeople(arrayOfFamousPeople) {
+    arrayOfFamousPeople.forEach((person, index) => {
+        console.log(`Searching...`);
+        console.log(`Found ${arrayOfFamousPeople.length} person(s) by the name ${person.first_name}`)
+        console.log(`- ${index + 1}: ${person.first_name} ${person.last_name}, born ${person.birthdate.toISOString().substring(0, 10)}`);
+    })
+}
